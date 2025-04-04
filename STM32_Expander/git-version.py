7c9538b1ab95c8192a51210f66e7571699f7f77f
Mitch Bradley <wmb@firmworks.com>
2025-04-04 11:22:54 -1000

    Added Release Builder action
    and auto-versioning

---
+++ STM32_Expander/git-version.py
@@ -0,0 +1,84 @@
+import subprocess
+import filecmp, tempfile, shutil, os
+
+# Thank you https://docs.platformio.org/en/latest/projectconf/section_env_build.html !
+
+gitFail = False
+try:
+    subprocess.check_call(["git", "status"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+except:
+    gitFail = True
+
+if gitFail:
+    tag = ""
+    rev = " (noGit)"
+    url = " (noGit)"
+else:
+    try:
+        tag = (
+            subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"], stderr=subprocess.DEVNULL)
+            .strip()
+            .decode("utf-8")
+        )
+    except:
+        tag = ""
+
+    # Check to see if the head commit exactly matches a tag.
+    # If so, the revision is "release", otherwise it is BRANCH-COMMIT
+    try:
+        subprocess.check_call(["git", "describe", "--tags", "--exact"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        rev = ''
+    except:
+        branchname = (
+            subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+            .strip()
+            .decode("utf-8")
+        )
+        revision = (
+            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+            .strip()
+            .decode("utf-8")
+        )
+        modified = (
+            subprocess.check_output(["git", "status", "-uno", "-s"])
+            .strip()
+            .decode("utf-8")
+        )
+        if modified:
+            dirty = "-dirty"
+        else:
+            dirty = ""
+        rev = "%s-%s%s" % (branchname, revision, dirty)
+
+    url = (
+        subprocess.check_output(["git", "config", "--get", "remote.origin.url"])
+        .strip()
+        .decode("utf-8")
+    )
+
+if tag == "":
+    git_info = rev
+else:
+    git_info = tag
+
+git_url = url
+
+provisional = "src/version.cx"
+final = "src/version.c"
+with open(provisional, "w") as fp:
+    fp.write('const char* fw_version   = \"' + git_info + '\";\n')
+    fp.write('const char* git_url      = \"' + git_url + '\";\n')
+
+if not os.path.exists(final):
+    # No version.cpp so rename version.cxx to version.cpp
+    os.rename(provisional, final)
+elif not filecmp.cmp(provisional, final):
+    # version.cxx differs from version.cpp so get rid of the
+    # old .cpp and rename .cxx to .cpp
+    os.remove(final)
+    os.rename(provisional, final)
+else:
+    # The existing version.cpp is the same as the new version.cxx
+    # so we can just leave the old version.cpp in place and get
+    # rid of version.cxx
+    os.remove(provisional)
